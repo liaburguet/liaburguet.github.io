9aaf2f6cf27666dfe75a844d88455fb2dc89ba04
E-Mud <E-Mud@users.noreply.github.com>
2021-12-17 10:44:44 +0000

    jekyll build from Action 4841484f1aa174ee10dd8867bb0ddd9f7488640c

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Morrer siendo queríu",
+        "excerpt":"Xunu de 2020 Dáben-y ascu los cocos, por eso nun quería enterrase. Quería volver ser polvu, tierra, y que la espardiéramos sobro un prau verde, bien verde, el más verde al pie de la sablera. «Acaba de morir», dixo la doctora per teléfono a metá d’abril, y sentí la mio...","categories": [],
+        "tags": [],
+        "url": "/morrer/",
+        "teaser": null
+      },{
+        "title": "Como los erizos",
+        "excerpt":"Al igual que su madre solía llamar a Granada la ciudad blanca, por la nieve que hasta ella derramaba la sierra y lo mucho que ambas gustaban de pasear cada recodo de esos senderos, ebrios de palidez, cada vez que llegaba la invernada. Ahora solo podía hacerlo sola, o quizás...","categories": [],
+        "tags": [],
+        "url": "/Como-los-erizos/",
+        "teaser": null
+      },{
+        "title": "La lloca",
+        "excerpt":"Suena nel vientu una canción de despidida. Los pasos clandestinos de toos aquellos neños entá repican na so memoria. Camín a la mar unos rin, otros cuerren, otros faen chancies, otros lloren. Nos sos suaños cientos de madres quedaron mirando a la mar, cientos de madres tendieron el so brazu...","categories": [],
+        "tags": [],
+        "url": "/La-lloca/",
+        "teaser": null
+      },{
+        "title": "Cartografía corporal",
+        "excerpt":"Mira bien enantes de dir per estes agües, hai daqué que tienes d’entender. Equí nun hai calma, solo una d’eses persones encegolaes cola mar que vive despeñada y que fai tratos col diañu. Carne y sangre so la bendición de la torbonada y la rabia. ¿Nun lo ves? Podríamos ser...","categories": [],
+        "tags": [],
+        "url": "/cartografia-corporal/",
+        "teaser": null
+      },{
+        "title": "Non cuntable",
+        "excerpt":"Diba tantu tiempu que nun m’unviabes un cantar que cada nota fízolo colos mios oyíos. Diba tantu tiempu que nun sentía la to voz, qu’al vierte nun sé qué voi faer. Morreme. Morreme. Morreme d’amor. Porque lo único que tengo ye esti amor pol amor. —Y nun tienes nin idea...","categories": [],
+        "tags": [],
+        "url": "/non-cuntable/",
+        "teaser": null
+      }]
